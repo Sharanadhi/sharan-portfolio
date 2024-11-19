e8bcc7d7c62a0ee725b22cc3b729eea58ea1ad3b
Sharan Athiyadath <sharanadhi@gmail.com>
2024-11-18 20:18:38 -0400

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+
+import Navbar from './components/Navbar/Navbar';
 import './App.scss'
 
 function App() {
   return (
-      <div>
-        <h1>Sharan&apos;s Portfolio</h1>
-      </div>
+      <>
+        <header>
+          <Navbar/>
+        </header>
+      </>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.scss'
+
+function Navbar() {
+  return (
+  <nav className='navbar'> 
+    <div className="navbar__logo"> 
+      <a href="#">Sharan Athiyadath</a> 
+    </div> 
+    <ul className="navbar__menu"> 
+      <li><a href="#" className='active'>Home</a></li> 
+      <li><a href="#">About</a></li> 
+      <li><a href="#">Projects</a></li> 
+      <li><a href="#">Contact</a></li> 
+    </ul> 
+  </nav>);
+}
+
+export default Navbar;
