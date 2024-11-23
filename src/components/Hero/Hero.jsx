ca7db900a2b319e4af5a3ea17033e64b9b00989d
Sharan Athiyadath <sharanadhi@gmail.com>
2024-11-23 02:18:10 -0400

    Fix for spacing issue in projects section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 
 import { FaLinkedin,FaGithub,FaSpotify,FaXTwitter,FaSquareYoutube,FaInstagram } from "react-icons/fa6";
-// import { Slide,Fade,Zoom } from "react-awesome-reveal";
+import { Slide,Fade,Zoom } from "react-awesome-reveal";
 
 import './Hero.scss';
 import sharan_headshot from '../../assets/images/Sharan_Athiyadath.jpg';
@@ -8,26 +8,26 @@ import sharan_headshot from '../../assets/images/Sharan_Athiyadath.jpg';
 function Hero(){
   return (
     <section className="hero">
-      {/* <Zoom duration={2000}> */}
+      <Zoom duration={2000}>
         <div className='hero__imgContainer'>
           <img className='hero__img' src={sharan_headshot} alt="Sharan's headshot" />
         </div>
-      {/* </Zoom> */}
-      {/* <Slide duration={2000}> */}
+      </Zoom>
+      <Slide duration={2000}>
         <h1 className="hero__heading">Sharan Athiyadath</h1>
-      {/* </Slide> */}
-      {/* <Slide duration={3000}> */}
+      </Slide>
+      <Slide duration={3000}>
         <h3 className="hero__subheading">Full Stack Developer</h3>
-      {/* </Slide> */}
+      </Slide>
         <ul className='hero__sociallinks'>
-        {/* <Fade delay={2000} cascade damping={0.1}> */}
+        <Fade delay={2000} cascade damping={0.1}>
           <li><a target="_blank" href="https://www.linkedin.com/in/sharan-athiyadath/"><FaLinkedin/></a></li>
           <li><a target="_blank" href="https://github.com/Sharanadhi"><FaGithub/></a></li>
           <li><a target="_blank" href="https://open.spotify.com/user/315qepx426u27kty6kxa5227dxoe?si=c8d4bc9ade6c4824"><FaSpotify/></a></li>
           <li><a target="_blank" href="https://x.com/sharanadhi1"><FaXTwitter/></a></li>
           <li><a target="_blank" href="https://youtube.com/@sharanadhi9196?si=zEaPk4dXBYvBKjHx"><FaSquareYoutube/></a></li>
-          <li><a target="_blank" href="#"><FaInstagram/></a></li>
-      {/* </Fade> */}
+          <li><a target="_blank" href=""><FaInstagram/></a></li>
+      </Fade>
         </ul>
     </section>
   )
